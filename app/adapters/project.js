439804693636156572b3e80af9a8aae1566f5c17
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-11-25 09:23:19 +0100

    added headers to project adapter, but commented them out

--- app/adapters/project.js
+++ app/adapters/project.js
@@ -2,4 +2,15 @@ import DS from "ember-data";
 
 var ProjectAdapter = DS.FixtureAdapter.extend({});
 
+/*var ProjectAdapter = DS.RESTAdapter.extend({  
+    host: 'https://group-collab-api.herokuapp.com',
+    namespace: 'api',
+    headers: Ember.computed(function() {
+        return {
+          "Authorization": "Basic " + Ember.get(document.cookie.match(/authToken\=([^;]*)/), "1"),
+          "ANOTHER_HEADER": "Some header value" 
+        };
+    }).volatile()
+});*/
+
 export default ProjectAdapter;
