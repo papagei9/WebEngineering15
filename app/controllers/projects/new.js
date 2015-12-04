d7097a5460ab74bc1a92cd44b21317f76981143b
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-12-04 15:30:21 +0100

    project create: removed project owner from member list

--- app/controllers/projects/new.js
+++ app/controllers/projects/new.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sessionAccount: Ember.inject.service('session-account'),
   actions: {
     create: function() {
       var project = this.get('model.project');
@@ -8,12 +9,12 @@ export default Ember.Controller.extend({
 
       var self = this;
 
-      var onSuccess = function(post) {
+      var onSuccess = function() {
         flashMessages.success('Project saved!', {timeout: 5000});
         self.transitionToRoute('project', project);
       };
 
-      var onFail = function(post) {
+      var onFail = function() {
         flashMessages.danger('Project could not save!', {timeout: 5000});
       };
 
--- app/routes/projects/new.js
+++ app/routes/projects/new.js
@@ -2,16 +2,18 @@ import Ember from 'ember';
 
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
+import _ from 'lodash/lodash';
+
 const { service } = Ember.inject;
 
 var ProjectNewRoute = Ember.Route.extend(AuthenticatedRouteMixin, {
-  sessionAccount: service('session-account'),
+    model: function() {
+    var _users = this.store.findAll('user');
 
-  model: function() {
-      return Ember.RSVP.hash({
-      users: this.store.findAll('user'), //TODO: exclude owner
-      project: this.store.createRecord('project')
-    });
+    return Ember.RSVP.hash({
+    users: _users,
+    project: this.store.createRecord('project')
+  });
   },
   actions: {
     create: function(model) {
