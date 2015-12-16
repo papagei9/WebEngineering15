163cec165afb836723aef08fb027eec9eafbc800
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-12-16 17:05:06 +0100

    delete button fixed

--- app/components/delete-button.js
+++ app/components/delete-button.js
@@ -2,31 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    confirm: function () {
-      var $btn, $confirm, el, height, offset, width;
-      el = this.get('element');
-      $btn = Ember.$('.btn-delete', el);
-      console.log($btn);
-      offset = $btn.offset();
-      console.log(offset);
-      $confirm = $btn.next();
-      height = $confirm.outerHeight();
-      width = $confirm.outerWidth();
-      $confirm.css('top', offset.top - height - 20);
-      $confirm.css('left', offset.left - width / 2 + 20);
-      return $confirm.fadeIn();
-    },
-    cancel: function () {
-      var $confirm, el;
-      el = this.get('element');
-      $confirm = Ember.$('.delete-confirm', el);
-      return $confirm.fadeOut();
-    },
     'delete': function () {
-      var $confirm, el;
-      el = this.get('element');
-      $confirm = Ember.$('.delete-confirm', el);
-      $confirm.fadeOut();
+      Ember.$('#delete_modal').modal('toggle');
       return this.sendAction('deleteAction', this.get('model'));
     }
   }
