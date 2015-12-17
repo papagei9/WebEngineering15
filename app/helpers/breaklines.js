ee14881cfbf352a1b0ed5797385dbeebef6b091f
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-12-17 21:38:58 +0100

    breaklines fix

--- app/helpers/breaklines.js
+++ app/helpers/breaklines.js
@@ -1,3 +1,5 @@
+import Ember from 'ember';
+
 export default Ember.Helper.helper(function(text) {
   text = Ember.Handlebars.Utils.escapeExpression(text);
   text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
