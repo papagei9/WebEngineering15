31d4de329aacb86aac3f0798cec168b5ae0820a6
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-12-17 12:11:04 +0100

    better account view

--- app/routes/account.js
+++ app/routes/account.js
@@ -5,5 +5,9 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(params) {
     return this.store.findRecord('user', Ember.$.cookie('user_id') );
+  },
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('projects', this.store.findAll('project'));
   }
 });
