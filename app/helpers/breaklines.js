5fd6f2debde0c6d96efc12c572a0f70d4f6fb60c
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-12-17 21:33:50 +0100

    text breaklines added

---
+++ app/helpers/breaklines.js
@@ -0,0 +1,5 @@
+export default Ember.Helper.helper(function(text) {
+  text = Ember.Handlebars.Utils.escapeExpression(text);
+  text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+  return new Ember.Handlebars.SafeString(text);
+});
